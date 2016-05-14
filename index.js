4b59e0a557a2dbf8f498efa0a7cc7d2d37a20dc2
Midge 't Hoen <midgethoen@gmail.com>
2016-05-14 22:39:42 +0200

    Using docker compose and depending on redis

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const redis = require("redis")
 const app = express();
+const client = redis.createClient(6379, 'redis')
 
+client.set('hits', 0)
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+  client.incr('hits', () => {
+    client.get('hits', (err, hits) => {
+      res.send(`Hello World! hit ${hits} times`);
+    })
+  })
 });
 
 app.listen(3000, function () {
